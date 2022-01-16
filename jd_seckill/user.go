ef467d4aa9975c58fff26ee75659e18c79b4dc0d
zengxu87 <350655@qq.com>
2022-01-16 17:05:06 +0800

    1

--- jd_seckill/user.go
+++ jd_seckill/user.go
@@ -93,6 +93,8 @@ func (this *User) TicketInfo(ticket string) (string,error) {
 	}
 	if gjson.Get(body,"returnCode").Int()==0 {
 		log.Println("二维码信息校验成功")
+		cookies:=resp.Cookies()
+		log.Println("cookies:", cookies)
 		return "",nil
 	}else{
 		log.Println("二维码信息校验失败")
